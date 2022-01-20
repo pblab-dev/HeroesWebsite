3e9696a1e8a90b65b259f91ae1a01e9fbb579545
Michael Nagy Teodoro <michaelnt8@gmail.com>
2022-01-20 20:44:40 -0300

    remove web em favor do ethers

--- src/web3.js
+++ src/web3.js
@@ -1,11 +1,9 @@
-// import Web3 from "web3";
 // import { ethers } from "ethers";
 
 export const connectWallet = async () => {
   if (window.ethereum) {
     const ethAccounts = await window.ethereum.send("eth_requestAccounts");
     console.log("Ethereum:", ethAccounts);
-    // window.web3 = new Web3(window.ethereum);
     return true;
   }
   return false;

5235e1e77546b86d548f3d5c6d0a61c25d914618
Michael Nagy Teodoro <michaelnt8@gmail.com>
2022-02-10 00:19:25 -0300

    converts header to bootstrap

--- src/pages/MarketPlace/List.jsx
+++ src/pages/MarketPlace/List.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import {
-  HStack,
-  Text,
-  VStack,
-  Select,
-  CheckIcon,
-  Image,
-  Box,
-} from "native-base";
+import { HStack, Text, VStack, Select, CheckIcon, Image } from "native-base";
 import styled from "styled-components";
 import Card from "../../components/Card";
 import arrowleft from "../../assets/arrow-left.png";
 import arrowright from "../../assets/arrow-right.png";
-import "./List.css";
 
 const CardTitle = styled(Text)`
   font-size: 22px;
@@ -67,15 +58,17 @@ function List() {
       <HStack w="100%" flexWrap="wrap" alignItems={{ md: "center" }}>
         {listData.map((item, index) => (
           <Card
-            w={{ sm: "100%", md: "100%", lg: "1/3" }}
-            px={5}
+            w={{ sm: "100%", xs: "100%", md: "100%", lg: "1/3" }}
+            pl={{ lg: 5, md: 0, sm: 0, xs: 0 }}
+            pr={{ lg: 5, xs: 10 }}
             py={5}
             {...(index === 0 && {
-              pl: { lg: 0, sm: 0, md: 0 },
-              pr: { sm: 10, md: 10 },
+              pl: { lg: 0, sm: 0, md: 0, xs: 0 },
+              pr: { sm: 10, md: 10, xs: 10 },
             })}
             {...(listData.length - 1 === index && {
               pr: { lg: 10, sm: 10, md: 10 },
+              pl: { lg: 10, sm: 0, md: 0 },
             })}
             key={`card-${index}`}
           >
--- src/pages/MarketPlace/MarketPlace.jsx
+++ src/pages/MarketPlace/MarketPlace.jsx
@@ -2,6 +2,7 @@ import { HStack, VStack } from "native-base";
 import styled from "styled-components";
 import Filter from "./Filter";
 import List from "./List";
+import { Container, Row, Col, Stack } from "react-bootstrap";
 
 const MediumButton = styled.button`
   font-size: 20px;
@@ -21,9 +22,9 @@ const InfoTags = styled.div`
 
 function MarketPlace() {
   return (
-    <VStack>
-      <HStack alignItems="center" justifyContent="space-around">
-        <HStack px="10" py="5" alignItems="center">
+    <Container>
+      <Stack direction="horizontal">
+        <Col className="d-flex justify-content-start">
           <MediumButton
             className="pixel-borders pixel-box--primary"
             style={{ marginRight: "50px" }}
@@ -33,21 +34,22 @@ function MarketPlace() {
           <MediumButton className="pixel-borders pixel-box--primary" disabled>
             Equipment
           </MediumButton>
-        </HStack>
-        <HStack alignItems="center">
+        </Col>
+
+        <Col className="d-flex justify-content-end">
           <InfoTags style={{ marginRight: "10px" }}>
             Floor Price: 2334 <span>BABY</span>
           </InfoTags>
           <InfoTags>
             7234 <span>MILK</span>
           </InfoTags>
-        </HStack>
-      </HStack>
-      <HStack flexWrap="wrap">
+        </Col>
+      </Stack>
+      <Row>
         <Filter />
         <List />
-      </HStack>
-    </VStack>
+      </Row>
+    </Container>
   );
 }
 
